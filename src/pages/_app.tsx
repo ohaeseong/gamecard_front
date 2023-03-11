652850813a765862aa216550dc64bd7ce1c35a50
jiÿüñ <choejiyunsp@gmail.com>
2023-03-11 22:33:39 +0900

    add global title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
+import Head from 'next/head'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>GAMECARD.GG</title>
+      </Head>
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-YBDDHMVVG2%22%3E"
