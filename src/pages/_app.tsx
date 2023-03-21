f2a704e0579b50fffa737c290cae8451a391b86d
ohaeseong <gotjd2720@gmail.com>
2023-03-21 23:21:30 +0900

    Update script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
-import Head from 'next/head'
-
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>GAMECARD.GG</title>
-        
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9902915188733725"
-          crossOrigin="anonymous"
-        />
       </Head>
 
-      
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9902915188733725"
+        crossOrigin="anonymous"
+      />
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-YBDDHMVVG2%22%3E"
